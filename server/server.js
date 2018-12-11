37853bde837376135b18f503d17175a7031a5d37
Akhil K <akhil.kn@pitsolutions.com>
2018-12-11 11:26:26 +0530

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const public_path = path.join(__dirname, '../public');
+
+app.use(express.static(public_path));
+
+app.listen(3000, () => {
+    console.log('Listening port 3000');
+})
